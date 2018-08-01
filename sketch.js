a3a2ac13a1fc740d834d2003b060ebc2cd239011
Kevin Boyle <papershack@gmail.com>
2018-07-31 21:48:48 -0700

    connect sketch.js to processing via node cli, create formula for pushing random img to twitter

--- sketch.js
+++ sketch.js
@@ -3,27 +3,49 @@ console.log('running sketch.js');
 
 var Twit = require('twit');
 var config = require('./config');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+let r = Math.floor(Math.random()*100);
 
 
 var T = new Twit(config);
 
-tweetBot;
-setInterval(tweetBot, 1000*60*5*25);
+tweetBot();
+setInterval(tweetBot, 1000*50);
 
 function tweetBot(){
-  let r = Math.floor(Math.random()*100);
-  let tweet = {
-  status: r
-}
-  T.post('statuses/update', tweet, function(err, data, response) {
-    if (err){
-      console.log("Post Unsuccessfull");
-    } else {
-      console.log("Status Posted Successfully");
-    }
-  });
+  var cmd = 'processing-java --sketch=`pwd`/test1 --run';
+  exec(cmd, processing);
 
+  function processing(){
+    var filename = 'test1/output.png';
+    var params = {
+      encoding: 'base64'
+    }
+    var b64content = fs.readFileSync(filename, params);
+    console.log('processing finished');
+
+    T.post('media/upload', { media_data: b64content }, uploaded );
+    function uploaded(err, data, response){
+      //this is where i will tweet!
+      var id = data.media_id_string;
+      let tweet = {
+        status: 'tropic' + r,
+        media_ids: [id]
+      }
+      T.post('statuses/update', tweet, tweeted);
+      function tweeted(err, data, response) {
+        if (err){
+          console.log("Post Unsuccessfull");
+        } else {
+          console.log("Status Posted Successfully");
+        }
+      };
+    }
+  }
 }
+
 
 
 
